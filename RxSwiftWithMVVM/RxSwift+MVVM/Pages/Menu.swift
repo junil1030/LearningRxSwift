16e678bb9db248f19b77830d9686b86b32163f6d
JunilSeo <dccrdseo@naver.com>
2025-04-09 12:01:28 +0900

    feat: Menu View Model 생성

---
+++ RxSwiftWithMVVM/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,14 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by 서준일 on 4/9/25.
+//  Copyright © 2025 iamchiwon. All rights reserved.
+//
+
+// View Model
+struct Menu {
+    var name: String
+    var price: Int
+    var count: Int
+}
