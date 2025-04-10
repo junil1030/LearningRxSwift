1aa4f515cb1908b75ba4702cd011b621afcb2f90
JunilSeo <dccrdseo@naver.com>
2025-04-10 17:21:37 +0900

    feat: DataSource 연결 끊고 Rx를 사용하여 Cell을 Binding하도록 변경

--- RxSwiftWithMVVM/RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
+++ RxSwiftWithMVVM/RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MenuViewController: UIViewController {
     // MARK: - Life Cycle
+    
+    let cellId = "MenuItemTableViewCell"
+    
+    let viewModel = MenuListViewModel()
+    var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel.menuObservable
+            .observeOn(MainScheduler.instance)
+            .bind(to: tableView.rx.items(cellIdentifier: cellId, cellType: MenuItemTableViewCell.self)) { index, item, cell in
+                cell.title.text = item.name
+                cell.price.text = "\(item.price)"
+                cell.count.text = "\(item.count)"
+                
+                cell.onChange = { [weak self] increase in
+                    self?.viewModel.changeCount(item: item, increase: increase)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.itemsCount
+            .map { "\($0)"}
+            .observeOn(MainScheduler.instance)
+            .bind(to: itemCountLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.totalPrice
+            .map { $0.currencyKR() }
+        // 이건 순환참조가 생기니까 weak self 해야하지만,
+//            .subscribe (onNext: {
+//                self.totalPrice.text = $0
+//            })
+        
+        // bind를 사용하면 순환참조가 생기지도 않고 한줄로 슈거할 수 있음
+        // UI를 건들이니까 메인 쓰레드에 넣어서
+            //.observeOn(MainScheduler.instance)
+            //.bind(to: totalPrice.rx.text)
+        
+        // 위와 같은 방식 말고도 UI에서 오류가 생겼을 경우를 맡아주는 함수가 존재
+            .asDriver(onErrorJustReturn: "")
+            .drive(totalPrice.rx.text)
+            .disposed(by: disposeBag)
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -37,27 +80,15 @@ class MenuViewController: UIViewController {
     @IBOutlet var totalPrice: UILabel!
 
     @IBAction func onClear() {
+        viewModel.clearAllItemSelections()
     }
 
     @IBAction func onOrder(_ sender: UIButton) {
         // TODO: no selection
         // showAlert("Order Fail", "No Orders")
-        performSegue(withIdentifier: "OrderViewController", sender: nil)
-    }
-}
-
-extension MenuViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        //performSegue(withIdentifier: "OrderViewController", sender: nil)
+        
+        viewModel.onOrder()
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
-
-        cell.title.text = "MENU \(indexPath.row)"
-        cell.price.text = "\(indexPath.row * 100)"
-        cell.count.text = "0"
-
-        return cell
-    }
 }
