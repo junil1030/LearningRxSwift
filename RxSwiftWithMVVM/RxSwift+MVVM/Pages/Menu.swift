7d0c84d6ded0e0431870c9d03a8617d7b946cdf8
JunilSeo <dccrdseo@naver.com>
2025-04-10 17:15:57 +0900

    feat: Menu 모델을 확장하여 MenuItem을 Menu 모델로 변환하는 함수 작성

--- RxSwiftWithMVVM/RxSwift+MVVM/Pages/Menu.swift
+++ RxSwiftWithMVVM/RxSwift+MVVM/Pages/Menu.swift
@@ -8,7 +8,14 @@
 
 // View Model
 struct Menu {
+    var id: Int
     var name: String
     var price: Int
     var count: Int
+}
+
+extension Menu {
+    static func fromMenuItems(id: Int, item: MenuItem) -> Menu {
+        return Menu(id: id, name: item.name, price: item.price, count: 0)
+    }
 }
